5577edd4cd91650ade577ae1c11c98781d17e142
S Cook <sfncook@gmail.com>
2014-03-14 17:54:21 -0700

    Sector dialog initially show

--- scripts/global.js
+++ scripts/global.js
@@ -1,10 +1,63 @@
-function initTBars( ) {
+function init( ) {
+	//Init T-Bars
 	for(var i=0; i<8; i++) {
 		var tbar = $("#tbar_prototype" ).clone().appendTo( "#tbar_container" );
 		var newId = "tbar_"+i;
 		tbar.attr("id",newId);
 	}
 	$("#tbar_prototype").hide();
+	
+	//Init Dialogs
+	//$("#windowDialogCancelBtn").hide();
+	$("#dialogContainer").click(hideAllDialogs);
+	//$("#dialogContainer").hide();
 }
 
-$( document ).ready(initTBars);
+//Call this to dismiss the dialogs
+function hideAllDialogs( ) {
+	$("#dialogContainer").hide();
+	$(".dialog").hide();
+	$(".dialog").children().hide();
+}
+
+$( document ).ready(init);
+
+
+
+
+
+var sectors = [
+	"REHAB",
+	"RESCUE",
+	"Sector 1",
+	"Sector 2",
+	"Sector 3",
+	"Sector 4",
+	"A Sector",
+	"B Sector",
+	"C Sector",
+	"D Sector",
+	"North Sector",
+	"East Sector",
+	"South Sector",
+	"West Sector",
+	"Interior",
+	"Roof",
+	"Ventilation",
+	"Salvage",
+	"Cust Service",
+	"Sprinkler",
+	"Stand-pipe",
+	"On Deck",
+	"IRIC",
+	"RIC",
+	"Medical",
+	"Safety",
+	"Staging",
+	"Lobby",
+	"Treatment",
+	"Evacuation",
+	"Resource",
+	"Transportation",
+	"Accountability",
+	"Triage"];
